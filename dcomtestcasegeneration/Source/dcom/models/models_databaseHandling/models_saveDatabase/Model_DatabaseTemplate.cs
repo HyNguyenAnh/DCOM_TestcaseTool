63d307ac22ec06f6862557c950f0d87ec611c818
Huy Nguyen Anh <bhg81hc@bosch.com>
2022-05-05 09:51:26 +0700

    Save button progress 30%

---
+++ dcomtestcasegeneration/Source/dcom/models/models_databaseHandling/models_saveDatabase/Model_DatabaseTemplate.cs
@@ -0,0 +1,41 @@
+﻿using dcom.controllers.controllers_middleware;
+using dcom.declaration;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dcom.models.models_databaseHandling.models_saveDatabase
+{
+    class Model_DatabaseTemplate
+    {
+        public static void SaveDatabase()
+        {
+
+            // Open the database_template file
+            DatabaseVariables.WbOutputDatabase = Controller_ExcelHandling.OpenExcel(DatabaseVariables.TemplatePath);
+
+            // Select the first sheet to push all data
+            DatabaseVariables.WsOutputDatabase = DatabaseVariables.WbOutputDatabase.Sheets[1];
+
+            // Push data to the database
+            //FillCommonSettingDatabase(DatabaseVariables.WsOutputDatabase);
+
+
+            // Save the database
+            Controller_ExcelHandling.SaveExcel(DatabaseVariables.PathOutputDatabase, DatabaseVariables.WbOutputDatabase);
+
+            // After Handling, close the testcase file
+            Controller_ExcelHandling.CloseExcel(DatabaseVariables.PathOutputDatabase, DatabaseVariables.WbOutputDatabase);
+
+        }
+
+        public static void FillCommonSettingDatabase(Worksheet Ws)
+        {
+            
+        }
+    }
+}
---
+++ dcomtestcasegeneration/Source/dcom/models/models_databaseHandling/models_saveDatabase/Model_SaveCommonSettingDatabase.cs
@@ -0,0 +1,38 @@
+﻿using dcom.declaration;
+using dcom.controllers.controllers_middleware;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dcom.models.models_databaseHandling.models_saveDatabase
+{
+    class Model_SaveCommonSettingDatabase
+    {
+        public static int[] startRowIndexDatabaseTable = DatabaseVariables.StartRowIndexDatabaseTables;
+        public static int[] startColumnIndexDatabaseTable = DatabaseVariables.StartColumnIndexDatabaseTables;
+
+        public static string sheetName = Controller_ServiceHandling.GetSheetNameOfService("0");
+
+        public static List<string[]> ProjectInformation_View()
+        {
+            List<string[]> dataTable = new List<string[]>();
+            string[] dataRow;
+            // Definition worksheet
+            DatabaseVariables.WsDatabase = DatabaseVariables.WbDatabase?.Sheets[sheetName];
+            Worksheet ws = DatabaseVariables.WsDatabase;
+            for (int rowIndex = startRowIndexDatabaseTable[8]; rowIndex <= 3; rowIndex++)
+            {
+                dataRow = new string[]
+                {
+                    ws.Cells[rowIndex, startColumnIndexDatabaseTable[8]   ].Text,  // Project Information
+                    dcom.views.views_ToolBar.View_Setting.ProjectInformation.ElementAt(rowIndex).Text,
+                };
+                dataTable.Add(dataRow);
+            }
+            return dataTable;
+        }
+    }
+}
